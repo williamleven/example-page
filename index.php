e8a8f3b3d74da4e97e4a61ff380d93fb0ccc3fe4
William <Gurgy@emycia.com>
2016-03-23 22:06:12 +0100

    Innehåll till startsidan

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
 	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-	<div class="pagepart startscreen"></div>
+	<div class="pagepart startscreen">
+		<h1>Välkommen till min sida</h1>
+		<div class="sitelink"></div>
+		<div class="sitelink"></div>
+		<div class="sitelink"></div>
+		<p>Klicka på en av rutorna eller scrolla ned för att se mer!</p>
+	</div>
 	<div class="pagepart"></div>
 	<div class="pagepart blue"></div>
 	<div class="pagepart"></div>
