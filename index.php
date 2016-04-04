fa264020848d0e30707cdb8f7125bfac937eac34
William <Gurgy@emycia.com>
2016-04-04 21:39:54 +0200

    lots of changes

--- index.php
+++ index.php
@@ -3,19 +3,35 @@
 <head>
 	<title>Min Webbsida</title>
 	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" href="genericons/genericons.css">
 </head>
 <body>
-	<div class="pagepart startscreen centercontainer flexvertical">
+	<div class="pagepart startscreen centercontent centercontent-vertical whitetext">
 		<h1>Välkommen till min sida</h1>
-		<div class="centercontainer">
-			<div class="sitelink"></div>
-			<div class="sitelink"></div>
-			<div class="sitelink"></div>
+		<div class="centercontent centercontent-dynamicrows">
+			<a href="#one">
+				<span class="genericon genericon-user sitelinkimages"></span>
+			</a>
+			<a href="#two">
+				<span class="genericon genericon-category sitelinkimages"></span>
+			</a>
+			<a href="#three">
+				<span class="genericon genericon-mail sitelinkimages" aria-hidden="true"></span>
+			</a>
 		</div>
-		<p>Klicka på en av rutorna eller scrolla ned för att se mer!</p>
+		<p>Klicka på en av ikonerna eller scrolla ned för att se mer!</p>
 	</div>
-	<div class="pagepart"></div>
-	<div class="pagepart blue"></div>
-	<div class="pagepart"></div>
+	<a name="one" class="anchor"></a>
+	<div class="pagepart centercontent centercontent-vertical">
+		<h2>Om mig</h1>
+	</div>
+	<a name="two" class="anchor"></a>
+	<div class="pagepart blue centercontent centercontent-vertical whitetext">
+		<h2>Portfolio</h1>
+	</div>
+	<a name="three" class="anchor"></a>
+	<div class="pagepart centercontent centercontent-vertical">
+		<h2>Kontakta mig</h1>
+	</div>
 </body>
 </html>
